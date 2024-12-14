1529ff8299f88f5a56998a90900dd67f5dd585fd
Владимир Солдатов <cbb110@rambler.ru>
2024-12-14 18:17:59 +0300

    Next Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, OnInit} from "@angular/core";
+import { HttpClientModule} from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { HttpService} from "./httpService";
+import {Film_short} from "./film";
+ 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: "app-root",
+    standalone: true,
+    imports: [HttpClientModule, FormsModule],
+    template: `<div>
+                    @if(film == undefined)
+                      {
+                      <input  name="title" [(ngModel)] = "title" />
+                      <button (click)="ClickButton()"> Нажми меня</button>
+                      }
+                      @else if (film.Response == "False") {
+                        <p>Ошибка в названии</p>
+                        <input  name="title" [(ngModel)] = "title" />
+                        <button (click)="ClickButton()"> Нажми меня</button>
+                      }
+                      @else {
+                    <p>Имя пользователя: {{film.Title}}</p>
+                    <p>Возраст пользователя: {{film.Year}}</p>
+                    <p>{{strolka}}</p>
+                    }
+                    
+               </div>`,
+    providers: [HttpService]
 })
-export class AppComponent {
-  title = 'videos';
+export class AppComponent { 
+    
+    film:Film_short| undefined;
+    title:string = '';   
+    strolka:string = 'sss';
+    constructor(private httpService: HttpService){}
+     ClickButton()
+     {
+      this.strolka= "https://www.omdbapi.com/?t=" + this.title.toLocaleLowerCase() +"&apikey=6b8a263a";
+      this.httpService.getData(this.strolka).subscribe({next:(data:any) => this.film = new Film_short(data.Title, data.Year, data.Response, data.Error)});
+    
+
+    }
 }
---
+++ src/app/film.ts
@@ -0,0 +1,40 @@
+export class Film_short
+{
+    constructor(title:string, year:string, response:string, error:string)
+    {
+     this.Title = title;
+     this.Year = year;
+     this.Response = response;
+     this.Error = error;
+     
+    }
+    public Title:string = '';
+    public Year:string = '';
+    public Rated:string = '';
+    public Released:string = '';
+    public Runtime:string = '';
+    public Genre:string = '';
+    public Director:string = '';
+    public Writer:string = '';
+    public Actors:string = '';
+    public Plot:string = '';
+    public Language:string = '';
+    public Country:string = '';
+    public Awards:string = '';
+    public Poster:string = '';
+    public Ratings:[] = [];
+    public Metascore:string = '';
+    public imdbRating:string = '';
+    public imdbVotes:string = '';
+    public imdbID:string = '';
+    public Type:string = '';
+    public DVD:string = '';
+    public BoxOffice:string = '';
+    public Production:string = '';
+    public Website:string = '';
+    public Response:string= '';   
+    public Error:string = '';
+
+     
+
+}
